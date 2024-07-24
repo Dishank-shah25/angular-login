205289fd11538f85dc5d402d7fe9b6a5290f6e57
Dishank-shah25 <dishankshah2000@gmail.com>
2024-07-24 11:22:08 +0530

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.prod.ts
+export const environment = {
+    production: true,
+    apiUrl: 'http://142.93.210.167:8000'
+  };
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+// src/environments/environment.ts
+export const environment = {
+    production: false,
+    apiUrl: 'http://142.93.210.167:8000'
+  };
